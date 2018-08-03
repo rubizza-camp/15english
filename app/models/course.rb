09d51f0ded74fef676cd2549c7fe519502ca4334
Polina <pollianna@tut.by>
2018-08-03 10:25:05 +0300

    Merged detected HEAD and commit v2

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,6 @@
 class Course < ApplicationRecord
-  belongs_to :theme_bloks
+  belongs_to :user
+  validates :user_id, presence: true # ??
+  validates :content, presence: true, length: { maximum: 20 }
+  #default_scope -> { order(created_at: :desc) }
 end
