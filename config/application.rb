f2a2f98cfb112d77a7505b352c1f6e10c59288fe
andreydro <andreydro14@gmail.com>
2018-08-05 23:58:28 +0300

    Add rubocop, reek, rails-best-practicesand flog

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
-require_relative 'boot'
+# frozen_string_literal: true
 
-require 'rails/all'
+require_relative "boot"
+
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,10 +13,10 @@ module Fifteenglish
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-   config.generators do |g|
-    g.test_framework :rspec
-    g.template_engine :slim
-   end
+    config.generators do |g|
+      g.test_framework :rspec
+      g.template_engine :slim
+    end
 
     config.i18n.default_locale = :en
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get '/:locale' => 'welcome#index'
-  root 'welcome#index'
-  scope ':locale', locale: /en|ru/ do
+  get "/:locale" => "welcome#index"
+  root "welcome#index"
+  scope ":locale", locale: /en|ru/ do
     devise_for :users
     resources :users, only: [:show]
   end
