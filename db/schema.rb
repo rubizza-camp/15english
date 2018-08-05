2064ef861952d9c04cb5636388af31a7c875e16f
Evenstein <star78@tut.by>
2018-08-05 17:58:32 +0300

    fix Bug after merge branches and add to User field "username"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_05_111432) do
+ActiveRecord::Schema.define(version: 2018_08_05_143341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2018_08_05_111432) do
     t.datetime "confirmation_sent_at"
     t.boolean "admin", default: false
     t.string "avatar"
+    t.string "username"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
