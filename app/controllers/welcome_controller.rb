3cace6502ed3e9ebd6e775373779f46dded67b09
Polina <pollianna@tut.by>
2018-08-07 15:35:03 +0300

    Add redirect for adnmin in static_page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
+  before_action :redirect_admin
+
   def index
+  end
+
+  private
+  def redirect_admin
+    redirect_to static_pages_admin_dashboard_path if current_user.admin?
   end
 end
