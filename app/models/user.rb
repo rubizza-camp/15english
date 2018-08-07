85f32729e359b3c0928bd84aa357da98647c15af
Polina <pollianna@tut.by>
2018-08-07 13:25:01 +0300

    Add model Image with polymorhic associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 # model for User
 class User < ApplicationRecord
+  has_many :images, as :imageable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
