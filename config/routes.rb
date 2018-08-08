fdae3b4a887f853db85f80d62cc08bf259c2b009
ilyuschitsvlad <ilyuschitsvlad@gmail.com>
2018-08-08 09:22:49 +0300

    Delete tests and magic comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@
 Rails.application.routes.draw do
   get "/:locale" => "welcome#index"
   root "welcome#index"
-  # get 'welcome#subjects'
   scope ":locale", locale: /en|ru/ do
     devise_for :users
     resources :users, only: [:show]
