913b6e943c08e6a7630ba90ddfe8dc8e8aecc309
andreydro <andreydro14@gmail.com>
2018-08-13 17:10:20 +0300

    Update 3 blocks associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 User.create!(email: "admin@admin.com",
              password:              "ffengadmin",
              password_confirmation: "ffengadmin",
+             username: "admin",
              admin: true,
              confirmed_at: Time.zone.now)
