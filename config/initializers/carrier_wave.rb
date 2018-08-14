1372b575005bf751f41c1708426dd3752a356c74
Polina <pollianna@tut.by>
2018-08-14 10:21:59 +0300

    Delete unnecessary comment in carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,6 @@ require "carrierwave/orm/activerecord"
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      # Configuration for Amazon S3
       provider:              "AWS",
       aws_access_key_i:      ENV["S3_ACCESS_KEY"],
       aws_secret_access_key: ENV["S3_SECRET_KEY"]
