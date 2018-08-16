97564391fd7c1dc9ecd1dfc8266013dc8a511eff
Polina <pollianna@tut.by>
2018-08-16 13:27:51 +0300

    fix mistake in view sub_test

--- app/controllers/sub_tests_controller.rb
+++ app/controllers/sub_tests_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class SubTestsController < ApplicationController
-  before_action :find_subject, only: [:show, :edit, :update, :destroy]
+  before_action :find_subject, only: [:show, :edit, :update, :destroy, :index]
 
   def find_subject
-    @subject = Subject.find(params[:id])
+    @subject = Subject.first #Subject.find(params[:id])
   end
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -4,7 +4,7 @@
 class Subject < ApplicationRecord
   belongs_to :course
   has_many :lessons
-  has_one :subject_sub_tests
-  has_one :sub_tests, through: :subject_sub_tests
+  has_one :subject_sub_test
+  has_one :sub_test, through: :subject_sub_test
   validates :title, presence: true
 end
