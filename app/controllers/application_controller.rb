027b2f0e4191aef0fab80a763bb419259086797e
andreydro <andreydro14@gmail.com>
2018-08-19 21:01:55 +0300

    add indentation in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
 
     def after_sign_in_path_for(resource)
       if current_user.admin
-      admin_root_path
+        admin_root_path
       else
         if current_user.courses.empty?
           level_path
