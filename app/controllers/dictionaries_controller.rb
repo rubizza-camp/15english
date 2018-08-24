5ba90aa8dab107f2d33b7f75447775fe476149d3
Evenstein <star78@tut.by>
2018-08-24 21:27:43 +0300

    fix Vadim's comments (View and Controller)

--- app/controllers/dictionaries_controller.rb
+++ app/controllers/dictionaries_controller.rb
@@ -1,59 +1,31 @@
 # frozen_string_literal: true
 
 class DictionariesController < ApplicationController
-  before_action :set_dictionary, only: [:show, :edit, :update]
+  before_action :set_dictionary, only: [:show]
 
   def index
+    @dictionary = Dictionary.new
   end
 
   def show
     @courses = Course.all
-    if params[:letter].nil?
-      @words = current_user.dictionary.words
-    else
-      @words = current_user.dictionary.words.where("en LIKE ?", "#{params[:letter]}%")
-    end
+    @words = current_user.dictionary.words
+    @words = @words.where("en LIKE ?", "#{params[:letter]}%") unless params[:letter].nil?
   end
 
   def new
-    @dictionary = Dictionary.new
   end
 
   def edit
   end
 
   def create
-    @dictionary = Dictionary.new(dictionary_params)
-
-    respond_to do |format|
-      if @dictionary.save
-        format.html { redirect_to @dictionary, notice: "Dictionary was successfully created." }
-        format.json { render :show, status: :created, location: @dictionary }
-      else
-        format.html { render :new }
-        format.json { render json: @dictionary.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   def update
-    respond_to do |format|
-      if @dictionary.update(dictionary_params)
-        format.html { redirect_to @dictionary, notice: "Dictionary was successfully updated." }
-        format.json { render :show, status: :ok, location: @dictionary }
-      else
-        format.html { render :edit }
-        format.json { render json: @dictionary.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   def destroy
-    @dictionary.destroy
-    respond_to do |format|
-      format.html { redirect_to dictionaries_url, notice: 'Dictionary was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
