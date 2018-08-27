efba84060b324b7b1cdeb8fc41df0d656e7eeede
Polina <pollianna@tut.by>
2018-08-27 08:57:44 +0300

    Fix some remarks in welcome_mailer

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -4,6 +4,6 @@
 class WelcomeMailer < ApplicationMailer
   def welcome_send(user)
     @user = user
-    mail to: user.email, subject: "Welcome to site", from: "example@example.com"
+    mail to: user.email, subject: "Welcome to site"
   end
 end
