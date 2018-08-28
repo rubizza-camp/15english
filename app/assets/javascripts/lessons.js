4722a52d8d6c208d4aa3b834658efa722bb6e164
andreydro <andreydro14@gmail.com>
2018-08-28 22:31:37 +0300

    Add json rendering

--- app/assets/javascripts/lessons.js
+++ app/assets/javascripts/lessons.js
@@ -1,57 +1,77 @@
 
 $(document).ready(function() {
-    // var mainBox = document.getElementsByClassName("main-lesson-box");
-    $("#question-box-3").show().siblings().hide()
+    var current_question = 1;
+    var collectionSize = document.getElementById("questions_wrapper").children.length;
 
-    // $('#questions_wrapper').not("#question-box-1").hide();
-    // $('id="button_1"').click(function() {
-    //     $('#wrapper').show();
-    //     $('id="button_2"').click(function() {
-    //         $('#wrapper').not(":eq(#box_5)").hide();
-    //     });
+    $(`#question-box-${current_question}`).show().siblings().hide();
 
+    $('#questions_wrapper').on('click', '.check_answer', function (e) {
+        e.preventDefault();
 
-        // $(mainBox).children().hide();
-    // $(mainBox[0]).children().show();
-    // $(mainBox).each(function(i,e) {
-    //     $(this).delay(i*400).fadeIn();
-    // });
-});
+        var question_id = document.getElementById("question_id").value;
+        var user_id = document.getElementById("user_id").value;
+        var learning_process_state_id = document.getElementById("learning_process_state_id").value;
+        var radios = document.getElementsByName('answer');
+        var checkedButton;
+        var checkButton = document.getElementsByClassName("check_answer");
 
-
-$('#check_answer').click(function (e) {
-    e.preventDefault();
-
-    var question_id = document.getElementById("question_id").value;
-    var user_id = document.getElementById("user_id").value;
-    var learning_process_state_id = document.getElementById("learning_process_state_id").value;
-
-    var radios = document.getElementsByName('answer');
-    var checkedButton;
-
-    for (var i = 0, length = radios.length; i < length; i++) {
-        if (radios[i].checked) {
-            checkedButton = radios[i].value;
-            break;
+        if (checkButton[0].value === "Next") {
+            var checkButton = document.querySelectorAll(".check_answer");
+            checkButton.forEach(function(e) {
+                e.value = "Check";
+            });
+            current_question += 1;
+            $(`#question-box-${current_question}`).show().siblings().hide();
+            return;
         }
-    }
 
-    $.ajax({
-        url: '/en/answer',
-        type: 'POST',
-        dataType: "json",
-        data: { answer: { answer: checkedButton, question_id: question_id, user_id: user_id, learning_process_state_id: learning_process_state_id } },
-        success: function (check) {
-            var messageBox = document.createElement('div');
-            messageBox.setAttribute("id", "message_box");
-            messageBox.innerHTML = check.message;
-
-            var boxHolder = document.getElementById("check-message");
-            boxHolder.appendChild(messageBox);
-
-            var checkButton = document.getElementById("check_answer");
-            checkButton.value = "Next";
+        for (var i = 0, length = radios.length; i < length; i++) {
+            if (radios[i].checked) {
+                checkedButton = radios[i].value;
+                break;
+            }
         }
-    })
-});
 
+        $.ajax({
+            url: '/en/answer',
+            type: 'POST',
+            dataType: "json",
+            data: { answer: { answer: checkedButton, question_id: question_id, user_id: user_id, learning_process_state_id: learning_process_state_id } },
+            success: function (check) {
+                var messageBox = document.querySelectorAll(".check-message");
+                var box = document.createElement('div');
+                box.id = "box";
+                if (check.message == 'Answer is correct') {
+                    var box = document.createElement('div');
+                    box.style.height = "50px";
+                    box.style.width = "100%";
+                    box.style.backgroundColor = "#ADFF2F";
+                    box.style.borderRadius = "10px";
+                    box.style.paddingTop = "12px";
+                    box.style.fontSize = "15px";
+                    box.style.fontWeight = "bold";
+                    box.innerHTML = check.message;
+                    messageBox[current_question - 1].appendChild(box);
+                } else {
+                    box.style.height = "50px";
+                    box.style.width = "100%";
+                    box.style.backgroundColor = "#FF0000";
+                    box.style.borderRadius = "10px";
+                    box.style.paddingTop = "12px";
+                    box.style.fontSize = "15px";
+                    box.style.fontWeight = "bold";
+                    box.innerHTML = check.message;
+                    messageBox[current_question - 1].appendChild(box);
+                }
+
+
+
+                var checkButton = document.querySelectorAll(".check_answer");
+                checkButton.forEach(function(e) {
+                    e.value = "Next";
+                });
+            }
+        });
+    });
+
+});
