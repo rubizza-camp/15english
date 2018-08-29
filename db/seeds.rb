4e5f7173c4adcf76648c6d3494401cc5682e7757
andreydro <andreydro14@gmail.com>
2018-08-29 23:11:57 +0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,5 @@ User.create!(email: "admin@admin.com",
              password_confirmation: "ffengadmin",
              username: "admin",
              admin: true,
+             slug: "admin",
              confirmed_at: Time.zone.now)
