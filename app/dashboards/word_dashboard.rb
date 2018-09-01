5d4092ab3be86cfd68de1b99a09473b148a772ba
Evenstein <star78@tut.by>
2018-09-01 10:16:55 +0300

    fix Dictionary dashboard

--- app/dashboards/word_dashboard.rb
+++ app/dashboards/word_dashboard.rb
@@ -14,6 +14,7 @@ class WordDashboard < Administrate::BaseDashboard
     id: Field::Number,
     en: Field::String,
     ru: Field::String,
+    usage: Field::String,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
   }.freeze
@@ -28,6 +29,7 @@ class WordDashboard < Administrate::BaseDashboard
     :id,
     :en,
     :ru,
+    :usage,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
@@ -37,6 +39,7 @@ class WordDashboard < Administrate::BaseDashboard
     :id,
     :en,
     :ru,
+    :usage,
     :created_at,
     :updated_at,
   ].freeze
@@ -48,6 +51,7 @@ class WordDashboard < Administrate::BaseDashboard
     :lesson,
     :en,
     :ru,
+    :usage,
   ].freeze
 
   # Overwrite this method to customize how words are displayed
