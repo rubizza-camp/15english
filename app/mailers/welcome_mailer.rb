eceb9ac7f3b6997ebd73ec5a315e8a303916b6e1
Evenstein <star78@tut.by>
2018-09-01 15:31:02 +0300

    fix mail image, label width

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -4,7 +4,7 @@
 class WelcomeMailer < ApplicationMailer
   def welcome_send(user)
     @user = user
-    attachments.inline["len9.jpg"] = File.read("#{Rails.root}/app/assets/images/len9.jpg")
+    attachments.inline["len9.jpg"] = File.read("#{Rails.root}/app/assets/images/len9_400-400.jpg")
     mail to: user.email, subject: "Welcome to fifteenglish"
   end
 end
